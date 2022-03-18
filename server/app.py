b743a6efaddc15c5fcbd1cba5415e67e5df18bb2
Anshuman Dikhit <anshuman.dikhit@gmail.com>
2022-03-17 21:03:02 -0700

    add second endpoint to route shortened url to original url

--- server/app.py
+++ server/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request
+from flask import abort, Flask, request, redirect
 
-from shortener import shorten
+from shortener import route_to_original, shorten
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, Hi There!'
+@app.route('/<url_hash>', methods=['GET'])
+def routeToUrl(url_hash):
+    original_url = route_to_original(url_hash)
+    if original_url:
+        return redirect("http://" + original_url, 301)
+    else:
+        return abort(404)
 
 
 @app.route('/shorten', methods=['POST'])
